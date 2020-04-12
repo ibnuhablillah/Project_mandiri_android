4c1f47fca8b46a74cd44bd1593948bb4e5067a41
Moh. Ibnu Hablillah <61903019+ibnuhablillah@users.noreply.github.com>
2020-04-12 11:17:24 +0700

    no message

--- JualBeliHp-TI17D4/app/src/main/java/id/my/donny/jualbelihp_ti17d4/model/Handphone.java
+++ JualBeliHp-TI17D4/app/src/main/java/id/my/donny/jualbelihp_ti17d4/model/Handphone.java
@@ -8,28 +8,27 @@ public class Handphone {
     public Handphone() {
         super();
     }
-    public  Handphone(Integer id, String nama, String harga) {
+    public Handphone(Integer id, String nama, String harga){
         this.id = id;
         this.nama = nama;
         this.harga = harga;
     }
-    public Integer getId() {
+    public Integer getId(){
         return id;
     }
     public void setId(Integer id) {
         this.id = id;
-
     }
-    public String getNama() {
-        return.nama;
+    public String getNama(){
+        return nama;
     }
     public void setNama(String nama) {
         this.nama = nama;
     }
-    public String getHarga() {
+    public String getHarga(){
         return harga;
     }
-    public void setHarga(String harga) {
+    public void setHarga(String harga){
         this.harga = harga;
     }
 }
